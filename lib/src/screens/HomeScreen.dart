c3896e9b41e208362554680e1c21127d8991888d
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2021-03-04 21:43:50 -0500

    login block finished
    Co-authored-by: Esteban R <XzzIsor@users.noreply.github.com>

--- lib/src/screens/HomeScreen.dart
+++ lib/src/screens/HomeScreen.dart
@@ -1,12 +1,29 @@
+import 'package:bloc_pattern/src/blocks/LoginProvider.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
 
+  
+
   @override
   Widget build(BuildContext context) {
+    final bloc = LoginProvider.of(context);
+
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sexo'),
+        title: Text('Home'),
+      ),
+
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("email: = " +  bloc.email),
+          Divider(),
+          Text("Contraseña: =  " + bloc.password),
+        ],
+
       ),
     );
   }
